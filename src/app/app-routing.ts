c66e7e2350077741a20d84c44010004413c9d2e0
Quan Tran <quantran081196@gmail.com>
2018-05-21 20:37:03 +0700

    initial setup

---
+++ src/app/app-routing.ts
@@ -0,0 +1,10 @@
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
+];
+
+export const AppRoutes = RouterModule.forRoot(appRoutes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
+import { AppRoutes } from './app-routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './authentication/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, LoginComponent],
+  imports: [BrowserModule, NgbModule.forRoot(), AppRoutes],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
